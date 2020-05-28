7d6be79261ef56cbd4f5b54dfaffb9d98c4ea07c
Griminov Alexander <sasha.griminov@mail.ru>
2020-05-28 20:53:26 +0300

    lab6

---
+++ lab6/ConsoleApplication1.cpp
@@ -0,0 +1,53 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <chrono>
+using namespace std;
+
+#define MAX 10
+mutex mt;
+
+void Producer(int &product)
+{
+	while (true)
+	{
+		if (product <= (MAX - 3))
+		{
+			mt.lock();
+			product += 3;
+			cout << "Producer " << product << endl;
+			mt.unlock();
+			this_thread::sleep_for(chrono::milliseconds(1500));
+			
+		}
+	}
+	
+}
+
+void Consumer(int &product)
+{
+	while (true)
+	{
+		if (product > 0)
+		{
+			mt.lock();
+			product--;
+			cout << "Consumer " << product << endl;
+			mt.unlock();
+			this_thread::sleep_for(chrono::milliseconds(1000));
+			
+		}
+	}
+}
+
+int main()
+{
+	int product = 0;
+	thread producer(Producer,std::ref(product));
+	thread consumer(Consumer, std::ref(product));
+
+	producer.join();
+	consumer.join();
+}
+
